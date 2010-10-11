63d182bb7f6619a37a9569a2ef35dc4e1c90388f
kenpachi <dgs32@yahoo.com>
2010-10-11 16:13:11 +0400

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   
   # get "users/new"
 
   match '/signup',  :to => 'users#new'
-
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+  
 #  get "pages/home"
 
 #  get "pages/contact"
